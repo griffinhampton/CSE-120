2551eaee4b6b062d48ee12bfd7a459016f0d0f4d
Griffin Hampton <gmhamp01@louisville.edu>
2025-01-30 17:43:37 -0500

    Final Commit

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
-#Griffin Hampton CSE-120 lab 2
+#Griffin Hampton CSE-120 Lab 2
+numberList=[]
 
-print("hello")
+animalList=[]
+i=0
+while i<5:
+    x = int(input("Enter a number: "))
+    numberList.append(x)
+    i+=1
+i=0
+while i<5:
+    y=input("Enter an animal: ")
+    animalList.append(y)
+    i+=1
+
+print(numberList)
+print(animalList)
